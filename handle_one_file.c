0026ca742876d26aa1562f548cb7d9bbc24741ef
Gilberto Plaza <gilberto.plaza@nexiona.com>
2022-04-05 10:49:33 +0200

    fix timing input/output

--- handle_one_file.c
+++ handle_one_file.c
@@ -562,7 +562,7 @@ int handle_one_file(char *inputFile, int print_alignment, int myid, int numprocs
         double avgtime_comm, avgtime_read, avgtime_prg, avgtime_es, avgtime_malloc, avgtime_lb;
         start = MPI_Wtime();
         MPI_Reduce(&communication_time, &avgtime_comm, 1, MPI_DOUBLE, MPI_SUM, 0,MPI_COMM_WORLD);
-        MPI_Reduce(&es_time, &avgtime_es, 1, MPI_DOUBLE, MPI_SUM, 0,MPI_COMM_WORLD);
+        //MPI_Reduce(&es_time, &avgtime_es, 1, MPI_DOUBLE, MPI_SUM, 0,MPI_COMM_WORLD);
         MPI_Reduce(&malloc_time, &avgtime_malloc, 1, MPI_DOUBLE, MPI_SUM, 0,MPI_COMM_WORLD);
         MPI_Reduce(&loadbalance_time, &avgtime_lb, 1, MPI_DOUBLE, MPI_SUM, 0,MPI_COMM_WORLD);
         MPI_Reduce(&handle_read_time, &avgtime_read, 1, MPI_DOUBLE, MPI_SUM, 0,MPI_COMM_WORLD);
@@ -575,7 +575,7 @@ int handle_one_file(char *inputFile, int print_alignment, int myid, int numprocs
             fprintf(stderr, "avg. time handle read: %f\n", avgtime_read/numprocs);
             fprintf(stderr, "avg. time malloc: %f\n", avgtime_malloc/numprocs);
             fprintf(stderr, "avg. time loadbalance: %f\n", avgtime_lb/numprocs);
-            fprintf(stderr, "avg. time handle I/O: %f\n", avgtime_es/numprocs);
+            fprintf(stderr, "avg. time handle I/O: %f\n", es_time);
             fprintf(stderr, "avg. time handle_one_file function: %f\n", avgtime_prg/numprocs);
         }
 
